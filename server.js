7c92a7c19f1d001f5de5a97f7795a3cb9cc391be
Yuriy Vyatkin <yuriyvyatkin@yandex.ru>
2021-09-19 18:27:45 +1000

    Change CORS politics

--- server.js
+++ server.js
@@ -2,42 +2,19 @@ const http = require('http');
 const Koa = require('koa');
 const Router = require('koa-router');
 const WS = require('ws');
-const cors = require('@koa/cors');
+const cors = require('koa2-cors');
 const koaBody = require('koa-body');
 const app = new Koa();
 
 // зададим базовые настройки политики CORS
-app.use(async (ctx, next) => {
-  const origin = ctx.request.get('Origin');
-  if (!origin) {
-    return await next();
-  }
-
-  const headers = { 'Access-Control-Allow-Origin': '*', };
-
-  if (ctx.request.method !== 'OPTIONS') {
-    ctx.response.set({ ...headers });
-    try {
-      return await next();
-    } catch (e) {
-      e.headers = { ...e.headers, ...headers };
-      throw e;
-    }
-  }
-
-  if (ctx.request.get('Access-Control-Request-Method')) {
-    ctx.response.set({
-      ...headers,
-      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH',
-    });
-
-    if (ctx.request.get('Access-Control-Request-Headers')) {
-      ctx.response.set('Access-Control-Allow-Headers', ctx.request.get('Access-Control-Request-Headers'));
-    }
-
-    ctx.response.status = 204;
-  }
-});
+app.use(
+  cors({
+    origin: '*',
+    credentials: true,
+    'Access-Control-Allow-Origin': true,
+    allowMethods: ['GET', 'POST'],
+  })
+)
 
 // инициализируем обработчик входящих сообщений
 app.use(koaBody());
